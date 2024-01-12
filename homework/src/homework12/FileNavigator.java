3b4778060a9d186bca1718d5447b929c2aeb72bb
vorynd <fullnamerealname@gmail.com>
2024-01-13 01:02:10 +0200

    homework 12

--- homework/src/homework12/FileNavigator.java
+++ homework/src/homework12/FileNavigator.java
@@ -6,9 +6,9 @@ import java.util.*;
 public class FileNavigator {
     private HashMap<String, ArrayList<FileData>> map = new HashMap<>();
 
-    public void add(FileData file, String path) throws InvalidPathException{
+    public void add(FileData file, String path) throws InvalidPathException {
         if (!Objects.equals(path, file.getPath())) {
-            throw new InvalidPathException(path, file.toString());
+            throw new InvalidPathException(file.toString(), path + " path do not match");
         }
         map.computeIfAbsent(path, key -> new ArrayList<>());
         map.get(path).add(file);
@@ -28,11 +28,11 @@ public class FileNavigator {
         return result;
     }
 
-    public void remove(String path){
+    public void remove(String path) {
         map.remove(path);
     }
 
-    public List<FileData> sortBySize(){
+    public List<FileData> sortBySize() {
         ArrayList<FileData> result = new ArrayList<>();
         map.forEach((key, value) -> result.addAll(value));
         result.sort(Comparator.comparingInt(FileData::getSize));
