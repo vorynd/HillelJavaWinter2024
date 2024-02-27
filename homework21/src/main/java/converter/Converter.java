d2bf8408fc1509f6541cba8b7f1f8857e8b6292d
vorynd <fullnamerealname@gmail.com>
2024-02-28 00:53:21 +0200

    homework 21

---
+++ homework21/src/main/java/converter/Converter.java
@@ -0,0 +1,125 @@
+package converter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+
+import java.io.File;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.util.*;
+
+public class Converter {
+    private final Path path;
+    private final Path convertedFolder;
+    private final String separator;
+
+    public Converter(String[] args) {
+        path = getDir(args);
+        separator = FileSystems.getDefault().getSeparator();
+        convertedFolder = Path.of(path + separator + "converted");
+    }
+
+    public void run() {
+        List<String> yamlList = new ArrayList<>();
+        List<String> jsonList = new ArrayList<>();
+        File converted = new File(convertedFolder.toUri());
+        converted.mkdir();
+        getFiles().forEach(str -> {
+            if (".json".equals(getFileExtension(str))) {
+                jsonList.add(str);
+            }
+            if (".yaml".equals(getFileExtension(str))) {
+                yamlList.add(str);
+            }
+        });
+        System.out.println(jsonList);
+        jsonList.forEach(this::convertJsonToYaml);
+        System.out.println(yamlList);
+        yamlList.forEach(this::convertYamlToJson);
+    }
+
+    private void convertJsonToYaml(String file) {
+        long startTime = System.currentTimeMillis();
+        try {
+            File oldFile = new File(file);
+            File convertedFile = new File(convertedFolder + separator
+                    + file.substring(file.lastIndexOf(separator), file.lastIndexOf(".")) + ".yaml");
+            convertedFile.createNewFile();
+
+            ObjectMapper jsonMapper = new ObjectMapper();
+            Object obj = jsonMapper.readValue(oldFile, Object.class);
+            ObjectMapper yamlMapper = new ObjectMapper(new YAMLFactory());
+            yamlMapper.writeValue(convertedFile, obj);
+            long elapsedTime = System.currentTimeMillis() - startTime;
+            log(oldFile, convertedFile, elapsedTime);
+        } catch (IOException ignored) {
+        }
+    }
+
+    private void convertYamlToJson(String file) {
+        long startTime = System.currentTimeMillis();
+        try {
+            File oldFile = new File(file);
+            File convertedFile = new File(convertedFolder + separator
+                    + file.substring(file.lastIndexOf(separator), file.lastIndexOf(".")) + ".json");
+            convertedFile.createNewFile();
+
+            ObjectMapper yamlMapper = new ObjectMapper(new YAMLFactory());
+            Object obj = yamlMapper.readValue(oldFile, Object.class);
+            ObjectMapper jsonMapper = new ObjectMapper();
+            jsonMapper.writeValue(convertedFile, obj);
+            long elapsedTime = System.currentTimeMillis() - startTime;
+            log(oldFile, convertedFile, elapsedTime);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private Path getDir(String[] args) {
+        if (args.length == 0) {
+            return FileSystems.getDefault().getPath("").toAbsolutePath();
+        } else return Path.of(args[0]);
+    }
+
+    private String getFileExtension(String str) {
+        int index = str.lastIndexOf(".");
+        return index == -1 ? null : str.substring(index);
+    }
+
+    private List<String> getFiles() {
+        return Arrays.stream(Objects.requireNonNull(new File(path.toUri()).listFiles())).map(File::toString).toList();
+    }
+
+    private void log(File oldFile, File newFile, long milis) throws IOException {
+        File log = new File(convertedFolder + separator + "log.txt");
+        if (!log.exists()) {
+            log.createNewFile();
+        }
+        String str = oldFile.getName() + " -> " + newFile.getName() + "; " + formatTime(milis) + " " + formatFileSize(oldFile) + " -> " + formatFileSize(newFile) + "; \n";
+        try (OutputStream outputStream = new FileOutputStream(log, true)) {
+            outputStream.write(str.getBytes());
+        }
+    }
+
+    private String formatFileSize(File file) {
+        long fileSizeBytes = file.length();
+        double fileSizeKB = (double) fileSizeBytes / 1024;
+        double fileSizeMB = (double) fileSizeBytes / (1024 * 1024);
+        if (fileSizeBytes < 1024) {
+            return  fileSizeBytes + " bytes";
+        } else if (fileSizeKB < 1024) {
+            return  String.format("%.2f KB", fileSizeKB);
+        } else return  String.format("%.2f MB", fileSizeMB);
+    }
+
+    private String formatTime(long milis) {
+        double elapsedTimeSeconds = milis / 1000.0;
+        return elapsedTimeSeconds + " seconds;";
+    }
+}
+
+
---
+++ homework21/src/main/java/converter/Run.java
@@ -0,0 +1,8 @@
+package converter;
+
+
+public class Run {
+    public static void main(String[] args)   {
+        new Converter(args).run();
+    }
+}
